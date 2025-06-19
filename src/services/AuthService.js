0c1e9df56ed0da11cb8ccf0aa90ced145c117b1d
anastasia_voroshilova <110638768+anastzn@users.noreply.github.com>
2025-06-20 00:02:05 +0300

    frontend

---
+++ src/services/AuthService.js
@@ -0,0 +1,51 @@
+import { apiAuth } from "../api/axios";
+
+
+export const loginRequest = async ({ login, password }) => {
+  try {
+    const res = await apiAuth.post("login", { Login: login, Password: password });
+    return res.data.user;
+  } catch (error) {
+    if (error.response?.status === 401) {
+      alert("Неверный логин или пароль");
+    }
+    throw error;
+  }
+};
+
+
+export const registerRequest = async ({ name, email, password }) => {
+  try {
+    const res = await apiAuth.post("register", {
+      Name: name,
+      Email: email,
+      Password: password,
+    });
+    return res.data.user;
+  } catch (error) {
+    if (error.response?.status === 400) {
+      alert("Такой email уже зарегистрирован");
+    }
+    throw error;
+  }
+};
+
+
+export const logoutRequest = async () => {
+  await apiAuth.post("logout");
+};
+
+
+export const checkAuthRequest = async () => {
+  try {
+    const res = await apiAuth.get("check");
+    return res.data;
+  } catch (error) {
+    if (error.response?.status === 401) {
+      if (window.location.pathname !== '/authh') {
+        window.location.href = '/authh';
+      }
+    }
+    throw error;
+  }
+};
